c9041ba9404a039b2e674faba9d949887f7ccc2b
Jachin Moss jachinmoss1998@gmail.com <jachinmoss1998@gmail.com>
2024-02-15 01:36:16 +0900

    commiting some edited files

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @profile = Profiles.all
+    @profile = Profile.all
   end
 
   def show
