dcfbfab95877928ffee63ccef1f990c6ae4d47a6
jan feans <janhummer01@gmail.com>
2024-02-16 19:25:23 +0100

    Update profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @profiles = Profile.all
@@ -7,6 +7,22 @@ class ProfilesController < ApplicationController
 
   def show
     @profile = Profile.find(params[:id])
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    @profile.update(profile_params)
+    redirect_to profile_path(@profile)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :email, :bio, :phone_number, :address, :profile_picture_url, :age)
   end
 
 end
