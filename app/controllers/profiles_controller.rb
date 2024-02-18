02fed73c1947f08d4f0f8982f9dcb1a1036e71a8
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-02-18 19:17:16 +0100

    Front-end redsign and new db:seed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,22 @@ class ProfilesController < ApplicationController
     @profiles = Profile.all
   end
 
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+
+    if @profile.save
+     redirect_to @profile, notice: 'Profile was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+
+    end
+  end
+
   def show
     @profile = Profile.find(params[:id])
   end
