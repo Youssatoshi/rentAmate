94fca0aa0bed6a6a65dc182a067d46e8ca6d36e4
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-02-24 17:32:28 +0100

    Dashboard design

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,5 +5,13 @@ class DashboardsController < ApplicationController
     @profile = current_user.profile || current_user.build_profile
     @bookings = current_user.bookings.order(start_time: :asc)
     @incoming_bookings = Booking.joins(:profile).where(profiles: { user_id: current_user.id }).order(start_time: :asc)
+    my_bookings
+  end
+
+  def my_bookings
+    @bookings = Booking.all # Adjust this as needed to fetch the bookings relevant to the user
+
+    # Count bookings by status
+    @bookings_by_status = @bookings.group_by(&:status).transform_values(&:count)
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "stimulus" # @3.2.2
+pin "chartkick" # @5.0.1
+pin "chart.js" # @4.4.1
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
